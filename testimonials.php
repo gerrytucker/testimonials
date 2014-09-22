e732a2b9b3c3c2ddf7e3a140509844b5edca4a1e
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2014-09-22 22:37:07 +0100

    Initial commit

---
+++ testimonials.php
@@ -0,0 +1,20 @@
+<?php
+/**
+Plugin Name: Testimonials
+Plugin URI: https://github.com/gerrytucker/testimonials
+Description: A WordPress Testimonials plugin
+Version: 0.1
+Author: Gerry Tucker
+Author URI: https://github.com/gerrytucker/testimonials
+GitHub Plugin URI: https://github.com/gerrytucker/testimonials
+*/
+
+class Testimonials {
+
+	public function __construct() {
+
+	}
+
+}
+
+$testimonials = new Testimonials();
